e2dc560ecda70007aded8792fbe654498275d69f
Inthrayuth Mahphol and Nick Lyman <inthrayuth@>
2016-05-04 09:20:51 -0700

    Pass first BDD instantiation test for Cuisine.java to create instance of Cuisine object correctly.

---
+++ src/main/java/Cuisine.java
@@ -0,0 +1,11 @@
+import org.sql2o.*;
+import java.util.Arrays;
+
+public class Cuisine {
+  private int id;
+  private String cuisine_type;
+
+  public Cuisine(String cuisine_type) {
+    this.cuisine_type = cuisine_type;
+  }
+}
---
+++ src/test/java/CuisineTest.java
@@ -0,0 +1,27 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
+public class CuisineTest {
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/best_restaurants_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteCuisinesQuery = "DELETE FROM cuisines *;";
+      String deleteRestaurantsQuery = "DELETE FROM restaurants *;";
+      con.createQuery(deleteCuisinesQuery).executeUpdate();
+      con.createQuery(deleteRestaurantsQuery).executeUpdate();
+    }
+  }
+
+  @Test
+  public void Cuisine_instantiatesCorrectly_true() {
+    Cuisine testCuisine = new Cuisine("BBQ");
+    assertTrue(testCuisine instanceof Cuisine);
+  }
+}
