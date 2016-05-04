a4feceaa5ac4f991c7a7a68606af69f4074220a5
Inthrayuth Mahphol and Nick Lyman <inthrayuth@>
2016-05-04 11:20:25 -0700

    Pass BDD test for correctly creating instance of Restaurant object.

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,10 @@
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class Restaurant {
+
+  public Restaurant(String restaurant_name, String restaurant_description) {
+    
+  }
+}
